a017f147010032a8315905efbb2b888c1b88f785
ooffyy <72337907+realooffyy@users.noreply.github.com>
2024-03-04 13:20:14 +0000

    auto open skycrypt link
    TODO: test if args are even given

--- commands/skycrypt.js
+++ commands/skycrypt.js
@@ -2,5 +2,7 @@
 import constants from "../utils/constants"
 
 register("command", (...args) => { 
-    ChatLib.chat(`${constants.PREFIX}https://sky.shiiyu.moe/stats/${args[0]}/${args[1]}`)
+    //ChatLib.chat(`${constants.PREFIX}https://sky.shiiyu.moe/stats/${args[0]}/${args[1]}`)
+    ChatLib.chat(`${constants.PREFIX}&aOpened ${args[0]}'s SkyCrypt!`)
+    java.awt.Desktop.getDesktop().browse(new java.net.URL(`https://sky.shiiyu.moe/stats/${args[0]}/${args[1]}`).toURI());
 } ).setName("skycrypt").setAliases("")
