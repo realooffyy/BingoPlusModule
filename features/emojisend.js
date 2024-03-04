cfc96d124910cb4b8e61fc1ae25cf7ffe98d0627
ooffyy <72337907+realooffyy@users.noreply.github.com>
2024-03-04 13:01:31 +0000

    added emojisend functionality

--- features/emojisend.js
+++ features/emojisend.js
@@ -1,6 +1,20 @@
 import settings from "../settings"
 import constants from "../utils/constants"
+import emojis from "../utils/emojis"
 
 register("messageSent", (message) => {
-    ChatLib.chat();
-}).setCriteria();
+    if (message.startsWith("/") && !message.startsWith("/pc") && !message.startsWith("/ac") && !message.startsWith("/gc")) return
+
+    let replaced = false
+
+    for (const key in emojis.emojis) {
+      if (message.includes(key)) {
+        replaced = true
+        message = message.replace(key, emojis.emojis[key]);
+      }
+    }
+    
+    if (!replaced) return
+    cancel(event)
+    ChatLib.say(message)
+})
