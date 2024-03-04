e263d0d90faaf7a289cdbc16b426b65638c75a41
ooffyy <72337907+realooffyy@users.noreply.github.com>
2024-03-04 13:10:01 +0000

    simplified if statement

--- features/emojisend.js
+++ features/emojisend.js
@@ -1,12 +1,11 @@
 import settings from "../settings"
-import constants from "../utils/constants"
 import emojis from "../utils/emojis"
 
 register("messageSent", (message) => {
-    if (message.startsWith("/") && !message.startsWith("/pc") && !message.startsWith("/ac") && !message.startsWith("/gc")) return
+    //if (message.startsWith("/") && !message.startsWith("/pc") && !message.startsWith("/ac") && !message.startsWith("/gc") && !message.startsWith("/cc")) return
+    if (message.startsWith("/") && !["/pc", "/ac", "/gc", "/cc", "/oc"].includes(message)) return
 
     let replaced = false
-
     for (const key in emojis.emojis) {
       if (message.includes(key)) {
         replaced = true
