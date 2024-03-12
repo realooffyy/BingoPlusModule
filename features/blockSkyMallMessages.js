e42e6862d5da2ab35672b7a131f9473e3873f666
ooffyy <muslg@mail.ru>
2024-03-12 23:03:00 +0000

    hide skymall message for the line with the buff

--- features/blockSkyMallMessages.js
+++ features/blockSkyMallMessages.js
@@ -1,10 +1,10 @@
 import settings from "../settings"
 
-const regex = /New day! Your Sky Mall buff changed!|You can disable this messaging by toggling Sky Mall in your \/hotm!/g
+const regex = /New day! Your Sky Mall buff changed!|You can disable this messaging by toggling Sky Mall in your \/hotm!|New buff: [Gain|Reduce|Increase].*/g
 
 register("chat", (e) => {
     if(!settings.block_sky_mall_messages) return
     cancel(e)
 }).setCriteria(regex)
 
-// https://regex101.com/r/vRJGV4/1
+// https://regex101.com/r/c0poYe/1
