86869df1eb9379e1a9891ec5bc34c0b2b079ba1c
ooffyy <muslg@mail.ru>
2024-03-12 23:05:23 +0000

    deleted:    features/fakeEmojis.js 	modified:   index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import "./utils/firstInstall"
 // Features
 import "./features/blockSkyMallMessages"
 import "./features/dialogueSkipper"
-import "./features/fakeEmojis"
+import "./features/chatFeatures"
 
 import "./features/bingo/bakerBlocker"
 import "./features/bingo/blockPartyTravelMessages"
@@ -22,8 +22,6 @@ import "./commands/centuryCakeIsland"
 ChatLib.chat("Bingo+ fully initialised!")
 
 /* TODO
- * - implement elementa to community goal display bc the renderer thing isn't getting past the inventory overlay
-        maybe not
  * - optimise dialogue skipper and figure out if im allowed to autoskip
  * - command for accessing bingo party commands
  * - reminder to do bingo stuff like cakes, experiments, fetchur, puzzler
@@ -37,6 +35,9 @@ ChatLib.chat("Bingo+ fully initialised!")
  * - remove bloomcore dependencies
  * - make the firstInstall thing clickable and runs /b+
  * - add button to community goal display to pin the render
+ * - bingo achievements
+ * - trapper display
+ * - splash display
  */
 
 /* IN PROGRESS
@@ -46,4 +47,8 @@ ChatLib.chat("Bingo+ fully initialised!")
 /* DONE
  * - message for first install
  * - command for visiting a cake hub
+ */
+
+/* CANCELLED (probably)
+ * - implement elementa to community goal display bc the renderer thing isn't getting past the inventory overlay
  */
