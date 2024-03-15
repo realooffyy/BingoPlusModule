e55dbbdf4f8fa87f83574a893fc7ba1fd773bd41
ooffyy <72337907+realooffyy@users.noreply.github.com>
2024-03-15 15:01:09 +0000

    made firstInstall message clickable

--- utils/firstInstall.js
+++ utils/firstInstall.js
@@ -5,14 +5,10 @@ const checkFirstInstall = () => {
     if (!data.firstTime) return
     data.firstTime = false
     data.save()
-    
-    const msgs = [
-        `&rThank you for installing ${constants.PREFIX}!`,
-        "&rRun the &6/b+ &rcommand to get started"
-        ]
 
     ChatLib.chat(`&6&m${ChatLib.getChatBreak(" ")}`)
-    msgs.forEach(a => ChatLib.chat(ChatLib.getCenteredText(a)))
+    ChatLib.chat(`&rThank you for installing ${constants.PREFIX}!`)
+    new TextComponent("&rRun the &6/b+ &rcommand to get started").setClick("run_command", "/b+")
     ChatLib.chat(`&6&m${ChatLib.getChatBreak(" ")}`)
 }
 
