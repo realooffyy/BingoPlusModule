fcdffd0a8c12d56e9d2603265b273a8dc54c2469
ooffyy <muslg@mail.ru>
2024-04-01 12:55:06 +0100

    fixed hotm level up detection

--- features/bingo/copyAchievements.js
+++ features/bingo/copyAchievements.js
@@ -2,7 +2,7 @@ import Settings from "../../settings"
 import constants from "../../utils/constants"
 import Bingo from "../../utils/Bingo";
 
-const regex = /(HEART OF THE MOUNTAIN.*|BINGO GOAL COMPLETE!.*|Your Bingo leveled up to level 100!)/g 
+const regex = /(  HEART OF THE MOUNTAIN TIER 1.*|BINGO GOAL COMPLETE!.*|Your Bingo leveled up to level 100!)/g 
 // https://regex101.com/r/nHf4KU/2
 
 register("chat", (event) => {
