da6310ddd2f20144ef948f5da78637d517d23a43
ooffyy <farmiow074@gmail.com>
2024-04-07 01:09:50 +0100

    rats rats we are the rats

--- features/world/rats.js
+++ features/world/rats.js
@@ -1,11 +1,10 @@
 import RenderLib from "../../../RenderLib/index.js"
-import Settings from "../../settings.js"
-import Skyblock from "../../utils/Skyblock.js"
+import Settings from "../../settings"
+import Skyblock from "../../utils/Skyblock"
 import { rats } from "../../utils/constants"
 
 register("renderWorld", () => {
-    if (Settings.ratWaypoints) return
-    if (Skyblock.area !== 'Hub') return
+    if (!Settings.ratWaypoints || Skyblock.area !== 'Hub') return
     rats.forEach(element => {
         RenderLib.drawEspBox(element[0], element[1], element[2], 1, 1, 1, 0, 0, 1, true)  
     })
